819e10bba5c372db924dc2bc6f6815c7b5930aa8
skogix <skogix@gmail.com>
2021-02-27 17:04:03 +0100

    basic gameloop

--- ConsoleUI/Program.fs
+++ ConsoleUI/Program.fs
@@ -2,6 +2,8 @@
 
 open System
 open Chess
+open Board
+open State
 
 let getGlyph (piece:Piece option) =
   match piece with
@@ -24,36 +26,27 @@ let getGlyph (piece:Piece option) =
       | Rook ->   '♜'
       | Queen ->  '♛'
       | King ->   '♚'
-let printBoard (board:Board): unit =
-  for square in board.Squares do
+let printState (state:State): unit =
+  Console.Clear()
+  for square in state.Board.Squares do
     let x,y = square.Position
+    Console.SetCursorPosition(x,y)
     printf "%c" (getGlyph square.Piece)
-    if y % 8 = 0 then printf "\n"
-  printfn ""
-//  for square in board.Squares do
-//    let x,y = square.Position
-//    Console.SetCursorPosition(x,y)
-//    printf "%c" (getGlyph square.Piece)
-//  Console.SetCursorPosition(1, 15)
-//  Console.WriteLine()
-
+    Console.SetCursorPosition(15, 1)
+    printf "%A" state.History.Head
+  Console.SetCursorPosition(1, 15)
+let rec gameLoop (agent:State.Agent) =
+  match Console.ReadKey(true).Key with
+    | ConsoleKey.N -> agent.SendCommand (LogText "command N")
+    | _ -> agent.SendCommand (LogText "hittade inte commandet")
+  gameLoop agent
 [<EntryPoint>]
 let main argv =
       
-  let agent = Chess.State.Agent()
-  let (skogix:Listener) = printBoard
-  agent.RegisterListener skogix
-  agent.SendCommand (LogText "Testar1")
-  agent.SendCommand (LogText "Testar2")
-//  agent.RegisterListener skogix
-//  let huhu = agent.GetHistory
+  let agent = State.Agent()
+  agent.State printState
+  gameLoop agent
+//  agent.State printState
+//  agent.SendCommand (LogText "Uppdaterar state")
   
-  Console.WriteLine()
-  let board = Board()
-//  printBoard board
-  Console.ReadKey() |> ignore
-  agent.SendCommand (LogText "Uppdaterar state")
-  
-    
-  Console.ReadKey() |> ignore
   0 // return an integer exit code
---
+++ Core/Domain.fs
@@ -0,0 +1,102 @@
+﻿namespace Chess
+
+open System.Collections.Generic
+type Row = int
+type Col = int
+type Position = (Row * Col)
+type Color = | White | Black
+type PieceType =
+  | Pawn
+  | Bishop
+  | Knight
+  | Rook
+  | Queen
+  | King
+type Command =
+  | LogText of string
+  | PrintHistory
+  | GetHistory of AsyncReplyChannel<Command list>
+type Piece = {
+  Color: Color
+  PieceType: PieceType
+}
+type Square =
+  {
+    Position: (int * int)
+    Piece: Piece option
+  }
+
+module Helpers =
+  let indexToCol i = ((i - 1) / 8) + 1
+  let indexToRow i = ((i - 1) % 8) + 1
+  let squareToIndex (row, col) = (8 * (row - 1) + (col - 1)) + 1
+  let indexToSquare i = (indexToRow i, indexToCol i)
+module Board =
+  type Board () =
+    let getInitPieces index =
+      match (Helpers.indexToRow index, Helpers.indexToCol index) with
+      | 2, _ -> Some { Color = White; PieceType = Pawn }
+      | 7, _ -> Some { Color = Black; PieceType = Pawn }
+      | 1, x ->
+        match x with
+        | 1 | 8 -> Some {Color = White; PieceType = Rook}
+        | 2 | 7 -> Some {Color = White; PieceType = Knight}
+        | 3 | 6 -> Some {Color = White; PieceType = Bishop}
+        | 4     -> Some {Color = White; PieceType = Queen}
+        | 5     -> Some {Color = White; PieceType = King}
+        | _ -> failwith "inte en startpos"
+      | 8, x ->
+        match x with
+        | 1 | 8 -> Some {Color = Black; PieceType = Rook}
+        | 2 | 7 -> Some {Color = Black; PieceType = Knight}
+        | 3 | 6 -> Some {Color = Black; PieceType = Bishop}
+        | 4     -> Some {Color = Black; PieceType = Queen}
+        | 5     -> Some {Color = Black; PieceType = King}
+        | _ -> failwith "inte en startpos"
+      | _, _ -> None
+    let squares = [
+                     for index in [1..64] do
+                       {
+                         Position = (Helpers.indexToCol index, Helpers.indexToRow index)
+                         Piece = getInitPieces index
+                       }
+                     ]
+    member this.Squares = squares
+  type State = {
+    History: Command list
+    Board: Board
+  }
+
+module State =
+  open Board
+  type Listener = State -> unit
+  module State =
+    type Agent() =
+      let stateListeners = List<Listener>()
+      let updateListeners (state:State) =
+        for listener in stateListeners do
+          listener state
+      let initState: State = { History = []
+                               Board = Board() }
+      let mailbox = MailboxProcessor<Command>.Start(fun inbox ->
+        let rec loop (oldState:State) = async {
+          let! command = inbox.Receive()
+          let newState =
+            match command with
+            | LogText msg ->
+              {oldState with History = command :: oldState.History}
+            | PrintHistory ->
+              printfn "%A" oldState
+              oldState
+            | GetHistory channel ->
+              channel.Reply(oldState.History)
+              oldState
+          updateListeners newState
+          return! loop newState
+        }
+        loop initState
+        )
+      member this.SendCommand cmd = mailbox.Post cmd
+      member private this.PostAndReply channel = mailbox.PostAndReply channel
+      member this.GetHistory = mailbox.PostAndReply (fun replyChannel -> GetHistory(replyChannel))
+      member this.State f = stateListeners.Add f
